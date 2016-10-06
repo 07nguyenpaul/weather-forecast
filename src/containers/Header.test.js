ee8403fbf645319456c86be0e6a53e34fb77fc25
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-10-06 15:49:28 -0600

    Fixing tests

--- src/containers/Header.test.js
+++ src/containers/Header.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 
-import App from './App';
+import Header from './Header';
 
 describe('Weather Tracker', () => {
   it('renders the name of the app in <h1> tag', () => {
-    const wrapper = shallow(<App name="Weather Tracker" />);
+    const wrapper = shallow(<Header name="Weather Tracker" />);
     const title = <h1>Weather Tracker</h1>
 
     expect(wrapper.contains(title)).toEqual(true);
   });
 
   it('should have title of "Weather Tracker"', () => {
-    const wrapper = shallow(<App name="Weather Tracker" />);
+    const wrapper = shallow(<Header name="Weather Tracker" />);
 
     expect(wrapper.find('h1').text()).toEqual("Weather Tracker");
   })
