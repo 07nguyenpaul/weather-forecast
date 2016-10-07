30573557f9571bae95e8fc68bfa334d0ad419de4
Casey Metz <caseymetz@gmail.com>
2016-10-07 16:21:33 -0600

    add action creator file

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,6 @@ export const receiveForecast = (forecastData) => ({
   data: do something to the weater data
 });
 
-
 export const fetchZipForecast = forecastType, zip, key => dispatch => {
   let key = '391d9430997a4787c74ca34ca02f4dc1';
   return fetch(`http://api.openweathermap.org/data/2.5/${forecastType}?zip=${zip},us&APPID=${key}`)
