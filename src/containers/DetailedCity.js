6dd9b93ebdda5307560ce3856ca921061b605185
swanie21 <kswanie21@gmail.com>
2016-10-09 17:18:32 -0600

    store fake data

--- src/containers/DetailedCity.js
+++ src/containers/DetailedCity.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import store from '../index';
+import { dispatch } from 'redux';
 
-class DetailedCity extends Component {
-  render() {
+const DetailedCity = ({data}) => {
+
+    console.log(data)
     return (
       <div className="detailForecastWrapper">
         <section>5 day/3 hour forecast for selected city</section>
       </div>
     );
-  }
 }
 
 export default DetailedCity;
