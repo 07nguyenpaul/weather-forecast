0460eab8452758333f4e96d5b708466e7506a9e0
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-10-10 18:07:58 -0600

    working in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import { connect } from 'react-redux';
+// import App from '../components/App';
+import * as actionCreators from '../actions/actionCreators';
+import { bindActionCreators } from 'redux';
+// import { fetchByGPS } from '../actions/actionCreators';
 
-const App = (props) => {
-  return (
-    <div>
-      <Header />
-      { props.children }
-    </div>
-  );
-};
+function mapStateToProps(state) {
+  return state;
+}
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+class App extends Component {
+
+
+
+  getLatAndLong() {
+    // let currentPosition = {
+    //   latitude: null,
+    //   longitude: null
+    // };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let currentPosition = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+        return currentPosition;
+
+    });
+    } else {
+      console.error('navigator.geolocation object not found. No geolocation services available.')
+    }
+  }
+
+  // currentpos = this.getLatAndLong();
+
+  componentDidMount() {
+    this.getLatAndLong();
+    this.props.fetchByGPS('weather', )
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <div>{this.props.children}</div>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
