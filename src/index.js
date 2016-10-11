89bbd6a00fd14bfb01e03961ea7d849052390e61
Casey Metz <caseymetz@gmail.com>
2016-10-10 19:35:33 -0600

    clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+// import createLogger from 'redux-logger';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import exampleReducer from './reducers/index';
 import App from './components/App';
@@ -11,30 +11,20 @@ import Dashboard from './components/Dashboard';
 import PinnedCitiesList from './components/PinnedCitiesList';
 import DetailedForecast from './components/DetailedForecast';
 
-
 require('!style!css!sass!./styles/main.scss');
 
-import dummyData from './data/dummyData';
-// import {syncHistoryWithStore} from 'react-router-redux';
-
-const middleware = [ thunk, createLogger ];
+// const middleware = [ thunk, createLogger ];
 
 const enhancers = compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
 
-const defaultState = {
-  data: {}
-};
-
 const store = createStore(
   exampleReducer,
-  defaultState,
+  {},
   enhancers,
   applyMiddleware(thunk)
 );
-
-// export const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
@@ -47,6 +37,6 @@ render(
     </Router>
   </Provider>,
   document.getElementById('root')
-)
+);
 
 export default store;
